c2fbf4f5841973bbf1e857296f85942bfe6b23d1
vigiljes <vigiljes@msu.edu>
2023-05-03 06:26:29 -0400

    testDEEPLOIcommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React wit ME and MSU!
         </a>
       </header>
     </div>
